ab05d502126add7dc210f113c34f52bc22526393
ljubanka <lvpopl@gmail.com>
2016-05-18 18:15:39 +0300

    task finished commit

--- src/test/java/ua/net/itlabs/searchtest/GoogleSearchTest.java
+++ src/test/java/ua/net/itlabs/searchtest/GoogleSearchTest.java
@@ -13,8 +13,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
-public class GoogleSearchTest {
+import static org.junit.Assert.assertEquals;
 
+public class GoogleSearchTest {
     static WebDriver driver;
 
     @BeforeClass
@@ -29,40 +30,32 @@ public class GoogleSearchTest {
 
     @Test
     public void testSearchAndFollowLink() {
-
-
         driver.get("http://google.com/ncr");
         driver.findElement(By.name("q")).clear();
         driver.findElement(By.name("q")).sendKeys("Selenium automates browsers" + Keys.ENTER);
-
-        WebDriverWait wait = new WebDriverWait(driver, 6);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(sizeOf(By.cssSelector(".srg>.g"), 10));
-        wait.until(textToBePresent(By.cssSelector(".srg>.g:first-child"), "Selenium automates browsers"));
+        wait.until(textToBePresentInElementLocated(By.cssSelector(".srg>.g:first-child"), "Selenium automates browsers"));
 
-
-
-//        searchResults.first().shouldHave(text("Selenium automates browsers"));
-//        followNthLink(0);
-//        $("#header>h1>a").shouldHave(exactText("Browser Automation"));
-//        assertEquals("http://www.seleniumhq.org/", url());
+        followNthLink(0);
+        wait.until(textToBePresentInElementLocated(By.cssSelector("#header>h1>a"), "Browser Automation"));
+        assertEquals("http://www.seleniumhq.org/", driver.getCurrentUrl());
     }
 
-    public static ExpectedCondition<Boolean> textToBePresent(final By elementsLocator, final String text) {
+    public static ExpectedCondition<Boolean> textToBePresentInElementLocated(final By elementsLocator, final String text) {
         return new ExpectedCondition<Boolean>() {
-            private List<WebElement> elements;
-
+            private WebElement element;
 
             public Boolean apply(WebDriver driver) {
-                elements = driver.findElements(elementsLocator);
-                return elements.contains(text);
+                element = driver.findElement(elementsLocator);
+                return element.getText().contains(text);
             }
 
             public String toString() {
-                return String.format("\nlist: %s\n doesn't contain: %s\n", elements, text);
+                return String.format("\nelement: %s\n doesn't contain: %s\n", element, text);
             }
         };
     }
-
 
     public static ExpectedCondition<Boolean> sizeOf(final By elementsLocator, final int expectedSize){
         return new ExpectedCondition<Boolean>() {
@@ -81,12 +74,8 @@ public class GoogleSearchTest {
         };
     }
 
-//    public void search(String text) {
-//        $(By.name("q")).setValue(text).pressEnter();
-//    }
-//
-//    public void followNthLink(int index) {
-//        searchResults.get(index).$(".r>a").click();
-//    }
+    public void followNthLink(int index) {
+        driver.findElements(By.cssSelector(".srg>.g")).get(index).findElement(By.cssSelector(".r>a")).click();
+    }
 
 }
