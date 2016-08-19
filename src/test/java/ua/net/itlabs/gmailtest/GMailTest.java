f79987fcb54244230206b9a10149f5c1669e0c91
ljubanka <lvpopl@gmail.com>
2016-08-19 14:58:26 +0300

    tiny changes

--- src/test/java/ua/net/itlabs/gmailtest/GMailTest.java
+++ src/test/java/ua/net/itlabs/gmailtest/GMailTest.java
@@ -1,5 +1,6 @@
 package ua.net.itlabs.gmailtest;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import ua.net.itlabs.core.Configuration;
 import ua.net.itlabs.gmailtest.pages.GmailPage;
@@ -12,13 +13,19 @@ import static ua.net.itlabs.gmailtest.testdata.LoginData.email;
 import static ua.net.itlabs.gmailtest.testdata.LoginData.password;
 
 public class GMailTest extends BaseTest {
+
+    @BeforeClass
+        public static void setup() {
+        Configuration.timeout = 16;
+    }
+
     GmailPage gmail = new GmailPage(getWebDriver());
     MailsPage mails = new MailsPage(getWebDriver());
     MenuPage menu = new MenuPage(getWebDriver());
 
     @Test
     public void testSendAndSearchEmail()  {
-        Configuration.timeout = 16;
+
         gmail.vizit();
 
         gmail.logIn(email, password);
